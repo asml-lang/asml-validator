2a49c4b270efa6055ed4513bbb014154dd9b76a1
Saman Soltani <sam.soltani@gmail.com>
2020-09-20 17:25:59 +0200

    adding the validations

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,76 @@
 import asml from 'asml';
+import Ajv from 'ajv';
+import { v4 as uuidv4 } from 'uuid';
+import { Model } from './interfaces';
 
 export default class AsmlValidator {
+    private ajv = new Ajv();
+    public errors: object;
 
     constructor() {
+        this.validateAsml();
+    }
+
+    validateAsml() {
+        delete asml["$schema"];
+        try {
+            this.ajv.addSchema(asml);
+            this.errors = this.ajv.errors;
+            return this.errors === null;
+        } catch (error) {
+            console.log('validateAsml', error);
+            console.log('validateAsml', this.ajv.errors);
+            return false;
+        }
+    }
+
+    validateModel(model: any) {
+        if (typeof model == 'string') {
+            model = this.safeJsonParse(model);
+        }
+
+        model.$schema = asml.$id;
+        model.$id = "http://saman.dev/asml/schemas/schema-model.json?" + this.generateUUID();
+
+        this.ajv.addSchema(model);
+        this.errors = this.ajv.errors;
+
+        return this.errors === null ? model["$id"] : null;
 
     }
 
+    validate(model: any, state: any) {
+        if (typeof state == 'string') {
+            state = this.safeJsonParse(state);
+        }
+
+        const model_id = this.validateModel(model);
+        if (model_id !== undefined) {
+            this.ajv.validate(this.validateModel(model), state);
+            this.errors = this.ajv.errors;
+
+            return this.errors === null;
+        }
+
+    }
+
+    private generateUUID() {
+        return uuidv4();
+    }
+
+    private safeJsonParse(str: string) {
+        try {
+            return [
+                [], JSON.parse(str)
+            ];
+        } catch (error) {
+            return [
+                [{
+                    message: error
+                }]
+            ];
+        }
+    }
 
 }
 
--- src/test.ts
+++ src/test.ts
@@ -1,9 +1,20 @@
 import AsmlValidator from '.';
 
-const asmlValidator = new AsmlValidator();
+import sampleModel from 'asml/examples/sending-email.asm.json';
+// import sampleState from 'asml/examples/sending-email.json';
+
+const asml = new AsmlValidator()
+
+const sampleState = {
+  "from": "samanib",
+  "to": "dennis.wolters@upb.de",
+  "body": "this is a text"
+}
 
 async function main() {
-
+  const result = asml.validate(sampleModel, sampleState);
+  console.log('errors:', asml.errors);
+  console.log('result:', result)
 }
 
 main();
